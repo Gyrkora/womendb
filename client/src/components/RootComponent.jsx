b7ceee05e680398d14d63229625a25ada5443c96
Gyrkora <giselle.gallardo0210@gmail.com>
2024-05-27 22:34:01 -0300

    loading icon

--- client/src/components/RootComponent.jsx
+++ client/src/components/RootComponent.jsx
@@ -1,20 +1,43 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Categories } from '../pages/Categories';
+import { Loader } from './UI/Loader';
 
 const RootComponent = () => {
 	const [categories, setCategories] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		axios
-			.get('https://womendb-api.onrender.com/tasks/api/v1/categories/')
-			.then((response) => {
-				setCategories(response.data);
-			})
-			.catch((error) => console.error('Error fetching categories:', error));
-	}, []);
+	// useEffect(() => {
+	// 	setLoading(true);
+	// 	setInterval(() => {
+	// 		axios
+	// 			.get('https://womendb-api.onrender.com/tasks/api/v1/categories/')
+	// 			.then((response) => {
+	// 				setCategories(response.data);
+	// 				setLoading(false);
+	// 			})
+	// 			.catch((error) => console.error('Error fetching categories:', error));
+	// 		setLoading(false);
+	// 	}, 5000);
+	// }, []);
 
-	return <Categories categories={categories} />;
+	if (loading) {
+		return (
+			<div>
+				<Loader />
+			</div>
+		);
+	}
+
+	return (
+		<div>
+			{categories ? (
+				<Categories categories={categories} />
+			) : (
+				<p>No objects loaded</p>
+			)}
+		</div>
+	);
 };
 
 export default RootComponent;
