80f5ed7e5eb8083c6b53e1caf10dd64ca84c5ef5
gyrkora <gyrkora>
2024-11-20 15:15:20 -0300

    conectado a back

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -10,18 +10,18 @@ export const getAllTasks = () => {
 // return axios.get('http://localhost:8000/tasks/api/v1/tasks/');
 
 export const getAllCategoriesList = () => {
-	return axios.get('https://womendb-api.onrender.com/tasks/api/v1/categories/');
+	return axios.get('http://localhost:8000/tasks/api/v1/categories/');
 };
 
 export const getAllCategories = (categoryId) => {
 	return axios.get(
-		`https://womendb-api.onrender.com/tasks/api/v1/categories/${categoryId}/`
+		`http://localhost:8000/tasks/api/v1/categories/${categoryId}/`
 	);
 };
 
 export const getAllWomen = (categoryId) => {
 	return axios.get(
-		`https://womendb-api.onrender.com/tasks/api/v1/women/?category=${categoryId}`
+		`http://localhost:8000/tasks/api/v1/women/?category=${categoryId}`
 	);
 };
 
@@ -47,10 +47,10 @@ export const updateTask = (id, task) => {
 una forma de llamarla en los componentes:
 
 	 axios
-		 	.get(`http://127.0.0.1:8000/tasks/api/v1/women/?category=${categoryId}`)
-		 	.then((response) => {
-		 		setWomen(response.data);
-		 	})
+			  .get(`http://127.0.0.1:8000/tasks/api/v1/women/?category=${categoryId}`)
+			  .then((response) => {
+					setWomen(response.data);
+			  })
 
 			.catch((error) => console.error('Error fetching women:', error));
 */
